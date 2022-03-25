9f9c6cdcda536c8bd7102453c3461dcb7a02a59d
emeralddawns <93622526+emeralddawns@users.noreply.github.com>
2022-03-25 16:13:22 -0500

    queries

--- project2.sql
+++ project2.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS communities CASCADE;
 DROP TABLE IF EXISTS listing CASCADE;
 DROP TABLE IF EXISTS airbnb_loc CASCADE;
-
+DROP TABLE IF EXISTS population CASCADE;
 
 CREATE TABLE communities (
     community_area serial,
@@ -31,6 +31,21 @@ CREATE TABLE airbnb_loc (
     longitude float   NOT NULL
 );
 
+CREATE TABLE population (
+    community_area serial PRIMARY KEY NOT NULL,
+    neighborhood varchar(30)  NOT NULL,
+    population int   NOT NULL,
+	income money NOT NULL,
+    latinos float   NOT NULL,
+    blacks float   NOT NULL,
+	white float   NOT NULL,
+	asian float   NOT NULL,
+	other float   NOT NULL,
+		FOREIGN KEY (neighborhood) REFERENCES communities(neighborhood)
+);
+
+
+
 ALTER TABLE "airbnb_loc" ADD CONSTRAINT "fk_airbnb_loc_id" FOREIGN KEY("id")
 REFERENCES "listing" ("id");
 
@@ -41,3 +56,4 @@ REFERENCES "communities"
 select * from communities
 select * from listing
 select * from airbnb_loc
+select * from population
